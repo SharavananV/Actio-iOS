114d49e1b2ec97fafb596b93fa30dc2cbc5af849
sushmitha <sushmitha@knila.com>
2020-07-08 17:23:37 +0530

    Completed Push Notifications and Login API Integration

--- Actio/ViewControllers/Login/LoginViewController.swift
+++ Actio/ViewControllers/Login/LoginViewController.swift
@@ -15,19 +15,24 @@ class LoginViewController: UIViewController {
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var loginButton: UIButton!
+    
     var iconClick = true
     let button = UIButton(type: .custom)
     var urlString = String()
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         userNameTextField.setBorderColor(width: 1.0, color: AppColor.TextFieldBorderColor())
         passwordTextField.setBorderColor(width: 1.0, color: AppColor.TextFieldBorderColor())
         self.loginButton.applyGradient(colours: [AppColor.OrangeColor(),AppColor.RedColor()])
         
-       self.button.setImage(UIImage(named: "hidden-eye.png"), for: .normal)
-       self.navigationController?.setNavigationBarHidden(true, animated: false)
+        userNameTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        self.button.setImage(UIImage(named: "hidden-eye.png"), for: .normal)
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
         
         button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 0)
         button.frame = CGRect(x: CGFloat(self.passwordTextField.frame.size.width - 25), y: CGFloat(5), width: CGFloat(25), height: CGFloat(25))
@@ -35,10 +40,15 @@ class LoginViewController: UIViewController {
         passwordTextField.rightView = button
         passwordTextField.rightViewMode = .always
         
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
         view.addGestureRecognizer(tap)
         
     }
+    
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
@@ -49,12 +59,24 @@ class LoginViewController: UIViewController {
         self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
+   @objc func keyboardWillShow(notification: NSNotification) {
+    if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+
     @objc func secureIconClickAction() {
         if(iconClick == true) {
             self.passwordTextField.isSecureTextEntry = false
             button.setImage(UIImage(named: "visibility-eye.png"), for: .normal)
-            
-            
         } else {
             self.passwordTextField.isSecureTextEntry = true
             self.button.setImage(UIImage(named: "hidden-eye.png"), for: .normal)
@@ -64,21 +86,32 @@ class LoginViewController: UIViewController {
         
         iconClick = !iconClick
     }
+    
     @IBAction func signupButtonAction(_ sender: Any) {
         print("Signin Tapped!!")
         
     }
+    
     @IBAction func loginButtonAction(_ sender: Any) {
         apiCall(username: userNameTextField.text!, password: passwordTextField.text!,Mode: "3", deviceToken: UDHelper.getDeviceToken())
         
     }
+    
     func apiCall(username: String,password: String,Mode: String,deviceToken:String) {
         urlString = loginUrl
         AF.request(urlString, method: .post, parameters: ["username": username,"password": password,"Mode": Mode,"deviceToken": deviceToken],encoding:JSONEncoding.default, headers: nil).responseJSON {
             response in
             switch response.result {
-            case .success:
+            case .success (let data):
                 print(response,"fgfgfgfsg")
+                if let resultDict = data as? [String: Any], let successText = resultDict["msg"] as? String, successText == "Login Success"{
+                    print("Logged In")
+                    
+                }else {
+                   if let resultDict = data as? [String: Any], let invalidText = resultDict["msg"] as? String, invalidText == "Invalid Login"{
+                       print("Invalid Login")
+                   }
+                }
             case .failure(_):
                 print("JSON")
             }
@@ -88,5 +121,11 @@ class LoginViewController: UIViewController {
     }
     
     
+}
+extension LoginViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder();
+        return true;
+    }
 }
 
