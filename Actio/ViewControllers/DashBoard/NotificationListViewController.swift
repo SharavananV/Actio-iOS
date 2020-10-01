abccd45474a4221447f56d2fa5faac87718de7be
Arun Eswaramurthi <arun.e@knila.com>
2020-10-01 10:50:34 +0530

    Notifications main storyboard changes

---
+++ Actio/ViewControllers/DashBoard/NotificationListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationListViewController.swift
+//  Actio
+//
+//  Created by senthil on 01/10/20.
+//  Copyright © 2020 Knila. All rights reserved.
+//
+
+import UIKit
+
+class NotificationListViewController: UIViewController {
+
+	@IBOutlet var tableView: UITableView!
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Actio/ViewControllers/DashBoard/TableViewCells/NotificationListTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationListTableViewCell.swift
+//  Actio
+//
+//  Created by senthil on 01/10/20.
+//  Copyright © 2020 Knila. All rights reserved.
+//
+
+import UIKit
+
+class NotificationListTableViewCell: UITableViewCell {
+
+	@IBOutlet var timeLabel: UILabel!
+	@IBOutlet var titleLabel: UILabel!
+	@IBOutlet var notificationImageView: UIImageView!
+	
+	func configure() {
+		
+	}
+
+}
