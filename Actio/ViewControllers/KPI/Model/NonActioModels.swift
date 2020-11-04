2f1c4fff6ebb11a03a81753a8a54e364e905f7c5
Arun Eswaramurthi <arun.e@knila.com>
2020-11-04 13:03:35 +0530

    Non actio Initial commit

---
+++ Actio/ViewControllers/KPI/Model/NonActioModels.swift
@@ -0,0 +1,51 @@
+//
+//  NonActioModels.swift
+//  Actio
+//
+//  Created by KnilaDev on 04/11/20.
+//  Copyright © 2020 Knila. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - NonActioMasterResponse
+struct NonActioMasterResponse: ResponseType {
+	var errors: [ActioError]?
+	var msg: String?
+	var status: String?
+	
+	var result: NonActioMaster?
+}
+
+// MARK: - NonActioMaster
+struct NonActioMaster: Codable {
+	var countryState: [CountryState]?
+	var sports: [TournamentSport]?
+	var years: [Int]?
+}
+
+// MARK: - CountryState
+struct CountryState: Codable {
+	var countryID: Int?
+	var countryName: String?
+	var states: [KPIState]?
+	
+	enum CodingKeys: String, CodingKey {
+		case countryID = "country_id"
+		case countryName = "country_name"
+		case states
+	}
+}
+
+// MARK: - State
+struct KPIState: Codable {
+	var stateID: Int?
+	var stateName: String?
+	var countryID: Int?
+	
+	enum CodingKeys: String, CodingKey {
+		case stateID = "state_id"
+		case stateName = "state_name"
+		case countryID = "country_id"
+	}
+}
---
+++ Actio/ViewControllers/KPI/NonActioSportViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NonActioSportViewController.swift
+//  Actio
+//
+//  Created by KnilaDev on 04/11/20.
+//  Copyright © 2020 Knila. All rights reserved.
+//
+
+import UIKit
+
+class NonActioSportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
